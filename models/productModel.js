9a688fd29a1a7d638cfb3b4dbcbd08eb5bc271ca
Anshika Goel <anshika1307goyal@gmail.com>
2023-12-20 05:31:33 +0530

    initial
    code added

---
+++ models/productModel.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    breed: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: mongoose.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+    shipping: {
+      type: Boolean,
+    },
+    userId: {
+      type:mongoose.Schema.Types.ObjectId,
+      ref:"users",
+    }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Products", productSchema);
